4bc56c0934d08bae38bd9f9ac7c0ffc294a9047d
Diego Maciel <77207932+DiegoMacielDM@users.noreply.github.com>
2021-10-25 15:00:31 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from recomendacao import *
+
+print(euclidiana(avaliacoesUsuario, 'Pedro', 'Janaina'))
+print("------------------")
+print(euclidiana(avaliacoesFilme, 'Star Wars', 'Star Trek'))
+print("------------------")
+print(avaliacoesFilme['Star Wars'])
+print("------------------")
+print(getSimilares(avaliacoesUsuario, 'Pedro'))
+print("------------------")
+print(avaliacoesFilme, 'Star Wars')
+print("------------------")
+print(getRecomendacoes(avaliacoesUsuario, 'Leonardo'))
+print("------------------")
+print(getRecomendacoes(avaliacoesFilme, 'Star Trek'))
+
+
+
+
+
